1988b3a3629714276584921c150a6ee25448a23c
Melvin Debot <melvin@exceenis.com>
2020-12-16 14:39:15 +0100

    add date

--- App.js
+++ App.js
@@ -1,21 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Platform} from 'react-native';
+
+//Import components
+import Header from './src/components/_Shared/Header'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Header />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: Platform.OS == 'android' ? 25 : 0,
+    paddingLeft: 20,
+    paddingRight : 20
   },
 });
---
+++ src/components/_Shared/Header/index.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+const days = [
+  'Dimanche',
+  'Lundi',
+  'Mardi',
+  'Mercredi',
+  'Jeudi',
+  'Vendredi',
+  'Samedi'
+]
+
+const months = [
+  "Janv",
+  "Fev",
+  "Mars",
+  "Avril",
+  "Mai",
+  "Jun",
+  "Juillet",
+  "Aout",
+  "Septembre",
+  "Octobre",
+  "Novembre",
+  "Décembre",
+]
+
+export default function Header() {
+  const date = new Date();
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>{ days[date.getDay()] + " " + date.getDate() + " " + months[date.getMonth()] }</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 20,
+    marginBottom : 20
+  },
+  title: {
+    fontSize: 25,
+    fontWeight : "bold"
+  }
+})
